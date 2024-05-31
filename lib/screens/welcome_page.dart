23c7d3525f64ad593002c5d301dcfd312d3d9502
Sathiranga <sathirangak@gmail.com>
2024-05-31 14:36:02 +0530

    created welcome page UI & Code

---
+++ lib/screens/welcome_page.dart
@@ -0,0 +1,103 @@
+import 'package:blood_donor/provider/auth_provider.dart';
+import 'package:blood_donor/screens/register_screen.dart';
+import 'package:blood_donor/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Welcomescreen extends StatelessWidget {
+  const Welcomescreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 150, 13, 13), // Set the background color to red
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Color.fromARGB(255, 150, 13, 13), // Red background color
+                child: Center(
+                  child: Image.asset(
+                    "assets/welcome.jpg",
+                    height: 300,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.white, // White background color
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Let's Get Start",
+                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 150, 13, 13)),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: CustomButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RegisterScreen(),
+                              ),
+                            );
+                          },
+                          text: "Get Start",
+                          
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+
+  const CustomButton({Key? key, required this.onPressed, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 150, 13, 13),
+        minimumSize: Size(200, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 18), // Increase the font size of the button text
+      ),
+    );
+  }
+}
